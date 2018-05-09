d009d72f773630d89b432b63dab828ebcc98dba7
Marek Lewiński <lewinski.marek@outlook.com>
2018-05-09 14:27:10 +0200

    Created package org.netrdoid.model
    Implemented Host model

---
+++ app/src/main/java/org/netdroid/model/Host.java
@@ -0,0 +1,15 @@
+package org.netdroid.model;
+
+import java.net.Inet4Address;
+
+/**
+ * Created by lewin on 09.05.2018.
+ */
+
+public class Host {
+    private String hostName;
+    private Inet4Address ipAddress;
+    private byte[] macAddress;
+    private String macVendor;
+
+}
