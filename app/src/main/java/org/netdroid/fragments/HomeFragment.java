a6f4a458b86df26dc7461694acbe56259a949fb7
Marek Lewiński <lewinski.marek@outlook.com>
2018-05-10 11:15:46 +0200

    Created fragments controllers

---
+++ app/src/main/java/org/netdroid/fragments/HomeFragment.java
@@ -0,0 +1,29 @@
+package org.netdroid.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.netdroid.R;
+
+/**
+ * Created by lewin on 10.05.2018.
+ */
+
+public class HomeFragment extends Fragment {
+    public HomeFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
