ce26f960c1d5a71711bc2df598f7509cb6d8dfea
Marek Lewiński <lewinski.marek@outlook.com>
2018-05-10 11:14:48 +0200

    Updated Host model
    Added getters and setters

--- app/src/main/java/org/netdroid/model/Host.java
+++ app/src/main/java/org/netdroid/model/Host.java
@@ -12,4 +12,35 @@ public class Host {
     private byte[] macAddress;
     private String macVendor;
 
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public Inet4Address getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(Inet4Address ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public byte[] getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(byte[] macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public String getMacVendor() {
+        return macVendor;
+    }
+
+    public void setMacVendor(String macVendor) {
+        this.macVendor = macVendor;
+    }
 }
